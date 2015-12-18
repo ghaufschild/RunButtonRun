3c1b8444f1b4d15d3c78f676be4e9a437ce7cb9d
Garrett Haufschild <student@macbook-air-2.local>
2015-12-18 12:50:51 -0600

    Initial Commit

---
+++ Run Button Run/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Run Button Run
+//
+//  Created by Garrett Haufschild on 12/18/15.
+//  Copyright © 2015 Swag Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
