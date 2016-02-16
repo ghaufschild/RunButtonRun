de76484a21d8462031761b96675f67fd96c33b10
Garrett Haufschild <student@macbook-air-3.local>
2016-02-16 12:36:21 -0600

    Made grid and allowed player to move.

--- Run Button Run/GameController.swift
+++ Run Button Run/GameController.swift
@@ -14,11 +14,14 @@ class GameController: UIViewController {
     let screenHeight: CGFloat = UIScreen.mainScreen().bounds.height
     
     var highScores = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]     //Array of high scores
-    var currentTime = 0.0                  //Current time
-    var gameStarted = false         //Used to start game
+    var currentTime = 0.0               //Current time
+    var gameStarted = false             //Used to start game
+    var segments: CGFloat = 8                    //Number of segments
     
     var timeLabel: UILabel!         //Tells current time
     var bestTimeLabel: UILabel!     //Tells high score
+    var vertBars: [UILabel]!        //Holds Vertical Bars                   //******     MIGHT BE USELESS   ******
+    var horBars: [UILabel]!         //Holds Horizontal Bars
     
     var upArrow = UIButton(type: UIButtonType.System)           //move player up
     var downArrow = UIButton(type: UIButtonType.System)         //move player down
@@ -75,39 +78,107 @@ class GameController: UIViewController {
         gameHolder.backgroundColor = UIColor.grayColor()
         view.addSubview(gameHolder)
         
+        let wid = gameHolder.frame.width * (segments - 1)/(segments * segments)           //Used for easy access
+        
         //User
-        player = UIView(frame: CGRectMake(0, 0, gameHolder.frame.width * 0.18, gameHolder.frame.height * 0.18))
+        player = UIView(frame: CGRectMake(screenWidth * 0.05 + wid/7, screenHeight * 0.15 + wid/7, wid, wid))
         player.backgroundColor = UIColor.greenColor()
-        gameHolder.addSubview(player)
+        view.addSubview(player)
         
         //Computer
-        enemy = UIView(frame: CGRectMake(gameHolder.frame.width - gameHolder.frame.width * 0.18, gameHolder.frame.height - gameHolder.frame.height * 0.18, gameHolder.frame.width * 0.18, gameHolder.frame.height * 0.18))
+        enemy = UIView(frame: CGRectMake(gameHolder.frame.maxX - wid, gameHolder.frame.maxY - wid, wid, wid))
         enemy.backgroundColor = UIColor.redColor()
-        gameHolder.addSubview(enemy)
+        view.addSubview(enemy)
         
         //Up Arrow Button
         upArrow = UIButton(frame: CGRectMake(screenWidth * 0.4, screenWidth, screenWidth * 0.2, screenWidth * 0.2))
+        upArrow.addTarget(self, action: "moveUp", forControlEvents: UIControlEvents.TouchUpInside)
+        upArrow.backgroundColor = UIColor.blueColor()
         view.addSubview(upArrow)
         
         //Down Arrow Button
         downArrow = UIButton(frame: CGRectMake(screenWidth * 0.4, screenWidth * 1.4, screenWidth * 0.2, screenWidth * 0.2))
+        downArrow.addTarget(self, action: "moveDown", forControlEvents: UIControlEvents.TouchUpInside)
+        downArrow.backgroundColor = UIColor.blueColor()
         view.addSubview(downArrow)
         
         //Left Arrow Button
         leftArrow = UIButton(frame: CGRectMake(screenWidth * 0.2, screenWidth * 1.2, screenWidth * 0.2, screenWidth * 0.2))
+        leftArrow.addTarget(self, action: "moveLeft", forControlEvents: UIControlEvents.TouchUpInside)
+        leftArrow.backgroundColor = UIColor.blueColor()
         view.addSubview(leftArrow)
         
         //Right Arrow Button
         rightArrow = UIButton(frame: CGRectMake(screenWidth * 0.6, screenWidth * 1.2, screenWidth * 0.2, screenWidth * 0.2))
+        rightArrow.addTarget(self, action: "moveRight", forControlEvents: UIControlEvents.TouchUpInside)
+        rightArrow.backgroundColor = UIColor.blueColor()
         view.addSubview(rightArrow)
+        
+        //Bars
+        for(var i: CGFloat = 0; i <= segments; i++)
+        {
+            let labelVer = UILabel(frame: CGRectMake(gameHolder.frame.width/segments * i, 0, wid / (segments - 1), gameHolder.frame.height))
+            labelVer.backgroundColor = UIColor.blackColor()
+            gameHolder.addSubview(labelVer)
+            let labelHor = UILabel(frame: CGRectMake(0, gameHolder.frame.width/segments * i, gameHolder.frame.width, wid / (segments - 1)))
+            labelHor.backgroundColor = UIColor.blackColor()
+            gameHolder.addSubview(labelHor)
+        }
         
         //Main View
         view.backgroundColor = UIColor.blackColor()
     }
     
-    func move(xDirection: Int, yDirection: Int)
+    //Direction Methods
+    func moveUp()
     {
-        
+        move(0, yDirection: -1, name: "player")
+    }
+    func moveDown()
+    {
+        move(0, yDirection: 1, name: "player")
+    }
+    func moveLeft()
+    {
+        move(-1, yDirection: 0, name:  "player")
+    }
+    func moveRight()
+    {
+        move(1, yDirection: 0, name: "player")
+    }
+    
+    func move(xDirection: CGFloat, yDirection: CGFloat, name: String)       //Used to move any object
+    {
+        let unit: CGFloat = gameHolder.frame.width * 1/segments                //One square length
+        if(name == "player")                    //Player is being moved
+        {
+            print("Here")
+            if(validLoc(player.frame.minX + (xDirection * unit), xLocMax: player.frame.maxX + (xDirection * unit), yLocMin: player.frame.minY + (yDirection * unit), yLocMax: player.frame.maxY + (yDirection * unit)))
+            {
+                print("got here")
+                player.frame.offsetInPlace(dx: unit * xDirection, dy: unit * yDirection)
+            }
+        }
+        if(name == "enemy")                     //Enemy is being moved
+        {
+            if(validLoc(enemy.frame.minX + (xDirection * unit), xLocMax: enemy.frame.maxX + (xDirection * unit), yLocMin: enemy.frame.minY + (yDirection * unit), yLocMax: enemy.frame.maxY + (yDirection * unit)))
+            {
+                enemy.frame.offsetInPlace(dx: unit * xDirection, dy: unit * yDirection)
+
+            }
+        }
+    }
+    
+    func validLoc(xLocMin: CGFloat, xLocMax: CGFloat, yLocMin: CGFloat, yLocMax: CGFloat) -> Bool           //Check if location is valid
+    {
+        if(xLocMin >= gameHolder.frame.minX && xLocMax <= gameHolder.frame.maxX
+            && yLocMin >= gameHolder.frame.minY && yLocMax <= gameHolder.frame.maxY)
+        {
+            return true
+        }
+        print(xLocMin, gameHolder.frame.minX, xLocMax, gameHolder.frame.maxX)
+        print(yLocMin, gameHolder.frame.minY, yLocMax, gameHolder.frame.maxY)
+            return false
     }
     
     
